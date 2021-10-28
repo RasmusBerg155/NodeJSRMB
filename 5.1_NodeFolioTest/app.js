c336e8fdc1b45c7b9f3dd599f788b96dbd111856
Rasmus <Rasmusmberg@gmail.com>
2021-10-28 15:07:36 +0200

    small updates

--- 5.1_NodeFolioTest/app.js
+++ 5.1_NodeFolioTest/app.js
@@ -1,23 +1,27 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static("public"));
 
 /* Import and use routes */
-const projectsRouter = require("./routers/projects.js")
-app.use(projectsRouter.router)
+const projectsRouter = require("./routers/projects.js");
+app.use(projectsRouter.router);
+
+/* Read the HTML files */
+const fs = require("fs");
+const frontpage = fs.readFileSync("./public/pages/frontpage/frontpage.html", "utf8");
+
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + '/public/frontpage/frontpage.html')
-})
+    res.send(frontpage);
+});
 
 app.get("/projects", (req, res) => {
-    res.sendFile(__dirname + '/public/projects/projects.html')
-})
-
+    res.sendFile(__dirname + "/public/pages/projects/projects.html");
+});
 
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, (error) => {
-    console.log("Server is running on", PORT)
-})
+    console.log("Server is running on", PORT);
+});
