70f2e1517f7f7c18080fa10c0720d4869ee22e5b
Rasmus <Rasmusmberg@gmail.com>
2021-11-22 01:26:40 +0100

    typed added and scroll up button

--- 5._Nodefolio/public/frontpage/frontpage.js
+++ 5._Nodefolio/public/frontpage/frontpage.js
@@ -5,13 +5,39 @@ $(document).ready(function(){
         } else {
             $('.navbar').removeClass("sticky");
         }
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");    
+        }else{
+            $('.scroll-up-btn').removeClass("show");  
+        }
     });
+
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+    });
+
     // toggle menu/navbar script
-     $('.menu-btn').click(function(){
-       $('.navbar .menu').toggleClass("active");
-       $('.navbar i').toggleClass("active");
-     });
- 
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.navbar i').toggleClass("active");
+    });
+
+    // typing animation script
+    let typed = new Typed(".typing", {
+        strings: ["Developer", "Software engineer", "Designer", "Programmer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    let typed2 = new Typed(".typing-2", {
+        strings: ["Developer", "Software engineer", "Designer", "Programmer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
      // Work in progress counter, missing dependencies
     /* $('.counter').counterUp({
          delay: 10,
